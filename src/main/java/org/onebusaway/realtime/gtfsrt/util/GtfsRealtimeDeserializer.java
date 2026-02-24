342dace24481986e117b50c3d637fd0267c5a3e4
Lenny Caraballo <lennycaraballo@gmail.com>
2026-02-24 13:10:50 -0500

    Add check for JSON in deserializer

--- src/main/java/org/onebusaway/realtime/gtfsrt/util/GtfsRealtimeDeserializer.java
+++ src/main/java/org/onebusaway/realtime/gtfsrt/util/GtfsRealtimeDeserializer.java
@@ -8,6 +8,24 @@ import java.nio.charset.StandardCharsets;
 
 public class GtfsRealtimeDeserializer {
 
+    public static GtfsRealtime.FeedMessage parseFeedMessage(byte[] message) throws IOException {
+        if (isJson(message)) {
+            return parseFeedMessageFromJson(message);
+        } else {
+            return parseFeedMessageFromProtobuf(message);
+        }
+    }
+
+    private static boolean isJson(byte[] message) {
+        if (message == null || message.length == 0) return false;
+        // Skip any leading whitespace
+        for (byte b : message) {
+            if (b == ' ' || b == '\t' || b == '\r' || b == '\n') continue;
+            return b == '{';
+        }
+        return false;
+    }
+
     public static GtfsRealtime.FeedMessage parseFeedMessageFromJson(byte[] message) throws IOException {
         try {
             String jsonString = new String(message, StandardCharsets.UTF_8);
